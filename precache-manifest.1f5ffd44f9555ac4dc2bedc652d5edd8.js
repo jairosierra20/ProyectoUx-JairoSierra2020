cf445d4a4b74ea79d33a03aa1ee504e2294ffa4e
jairosierra20 <jairosierra204@hotmail.com>
2020-08-22 18:33:18 -0600

    Updates

---
+++ precache-manifest.1f5ffd44f9555ac4dc2bedc652d5edd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b582fb2e5fb2ca997efba4935c9efa6e",
+    "url": "/ProyectoUx-JairoSierra2020/index.html"
+  },
+  {
+    "revision": "a9faeee956f17de44ec8",
+    "url": "/ProyectoUx-JairoSierra2020/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ba1a6a79393379981533",
+    "url": "/ProyectoUx-JairoSierra2020/static/js/2.e4a6447b.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/ProyectoUx-JairoSierra2020/static/js/2.e4a6447b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9faeee956f17de44ec8",
+    "url": "/ProyectoUx-JairoSierra2020/static/js/main.56c3b822.chunk.js"
+  },
+  {
+    "revision": "1dbe802022296d56d43b",
+    "url": "/ProyectoUx-JairoSierra2020/static/js/runtime-main.14e45667.js"
+  }
+]);
